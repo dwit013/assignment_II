8c22f8e0062a97494475d2660f152f2046fe611b
Finjo Sherpa <buddhist.magus@gmail.com>
2018-11-20 17:37:02 +0545

    modified commit

--- FinjoAss2/Qno6.py
+++ FinjoAss2/Qno6.py
@@ -4,12 +4,10 @@ Sample List : ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
 Expected Output : ['Green', 'White', 'Black']
 
 '''
-list1 = [1,2,3,4,5,6,7,8,9]
+list1 = ['red','green','white','black','pink','yellow']
 
 print("List before items removed",list1)
 
-list1.pop(0)
-list1.pop(4)
-list1.pop(5)
 
-print("list after the removal of 0th 4th and 5th item: ",list1)
+
+print("list after the removal of 0th 4th and 5th item: ",list1[1:4])

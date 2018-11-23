7c0c82c3b5c7ad591c62320854092d71e1dc3162
shreejan <shreejan.sp@gmail.com>
2018-11-23 09:49:26 +0545

    assignment

---
+++ no_2.py
@@ -0,0 +1,11 @@
+
+
+string1=str(input("enter string"))
+l=len(string1)
+
+if(l<2):
+    print("Empty string.")
+
+else:
+    print("Result: ",string1[0],string1[1],string1[l-2],string1 [l-1])
+    
